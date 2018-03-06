87760fb30c6d28234eceb17888fb9c7de29a97ff
Obafemi Fadairo <ofpupp@princeton.edu>
2018-03-06 13:34:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
   
     get "/multiply/:num1/:num2" do
-    @user_name = params[:name]
+    @answer = params[:num1] * params[:num2]
     "Hello #{@user_name}!"
   end
 end
